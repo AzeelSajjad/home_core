7b3636d2f0fffd4eef77525e1230efe22da7b216
Azeel Sajjad <aasajjad05@gmail.com>
2025-06-11 14:34:29 -0400

    Finished the opra form generator with OpenAI integration

--- backend/app/template/opra_form.py
+++ backend/app/template/opra_form.py
@@ -1,8 +1,8 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from openai import OpenAI
 
-load_dotenv()
+load_dotenv(find_dotenv())
 client = OpenAI(api_key=os.getenv("API_KEY"))
 
 def opra_form():
@@ -12,3 +12,7 @@ def opra_form():
         input="Create an OPRA Request Form for requesting rent history records from a municipal housing authority",
     )
     return response.output_text
+
+if __name__ =="__main__":
+    response = opra_form()
+    print(response)
