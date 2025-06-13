58f00e1bd40f8af9beba1f145ba8dc09a9e83fbd
Azeel Sajjad <aasajjad05@gmail.com>
2025-06-13 16:00:41 -0400

    tested api endpoints

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
+from app.api.endpoints.opra import router as opra_router
 
 app = FastAPI()
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(opra_router, prefix="/opra")
