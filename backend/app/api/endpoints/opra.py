1abfbe083d7a31f5192d41aae2e4f014a16f767c
Azeel Sajjad <aasajjad05@gmail.com>
2025-06-13 15:06:21 -0400

    created API endpoint

---
+++ backend/app/api/endpoints/opra.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from app.templates.opra_form import opra_form
+
+router = APIRouter()
+
+@router.post("/generate")
+async def generate():
+    response = opra_form()
+    return {"form": response}
