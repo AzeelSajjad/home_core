0af5e82fd4d95dfdab3971ae933b411451b893c4
Azeel Sajjad <aasajjad05@gmail.com>
2025-06-16 17:15:25 -0400

    set up database connection

--- backend/app/models/database.py
+++ backend/app/models/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv, find_dotenv
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv(find_dotenv())
 db_url = os.getenv("DATABASE_URL")
 engine = create_engine(db_url)
+
+SessionLocal = sessionmaker(bind=engine, autoFlush=False, autocommit=False)
+Base = declarative_base()
