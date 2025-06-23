f416604ba4ae77dc146269b649305599aa2b5d6c
Azeel Sajjad <aasajjad05@gmail.com>
2025-06-23 13:29:56 -0400

    not using the openAI API for form generation and auto population

--- backend/app/api/endpoints/opra.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from backend.app.services.opra_form import opra_form
-
-router = APIRouter()
-
-@router.post("/generate")
-async def generate():
-    response = opra_form()
-    return {"form": response}
--- backend/app/services/opra_questionaire.py
+++ backend/app/services/opra_questionaire.py
@@ -1,6 +1,6 @@
 from models.opra_model import OPRAQuestionnaireData
 from fastapi import HTTPException
-from services import opra_form
+
 
 def process_questionnaire(questionnaire_data: OPRAQuestionnaireData):
     optional_fields=['mi', 'fax']
@@ -11,4 +11,5 @@ def process_questionnaire(questionnaire_data: OPRAQuestionnaireData):
             raise HTTPException(status_code=400, detail=f"{field_name} is required")
     questionnaire_data.phone = questionnaire_data.phone.replace('-', '').replace(' ', '')
     questionnaire_data.state = questionnaire_data.state.upper().strip()
-        
+    response = opra_form(questionnaire_data)
+    
