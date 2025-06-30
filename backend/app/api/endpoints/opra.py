89026672c83af67a15d24beacf0dbb503d8dfcb8
Azeel Sajjad <aasajjad05@gmail.com>
2025-06-30 14:44:32 -0400

    created api endpoints for the function

--- backend/app/api/endpoints/opra.py
+++ backend/app/api/endpoints/opra.py
@@ -1,2 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from models.opra_model import OPRAQuestionnaireData
+from sqlaclchemy.orm import Session
+from database import get_db
+from services.opra_services import process_questionnaire, save_db, fill_opra_form
+
+app = FastAPI()
+
+@app.post("/submit-opra")
+def submit_opra(data: OPRAQuestionnaireData, db: Session = Depends(get_db)):
+    process_questionnaire(data)
+    save_db(data, db)
+    fill_opra_form(data)
+    return {"message": "Form submitted"}
