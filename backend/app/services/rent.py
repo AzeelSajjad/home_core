114a0567383f982b959f36fd818ac66ea8f4b872
Azeel Sajjad <aasajjad05@gmail.com>
2025-08-31 22:02:03 -0400

    added new features to the rent calculator(illegal rent raises)

--- backend/app/services/rent.py
+++ backend/app/services/rent.py
@@ -1,7 +1,18 @@
-from datetime import datetime, timedelta    
+from datetime import datetime 
 
-def rent_calculator(old_rent, new_rent):
-    allowed_rent = old_rent * 1.04
+def rent_calculator(old_rent, new_rent, date: datetime, cpi_rates):
+    year = date.year
+    month = date.month
+    illegal_date1 = datetime(2020, 4, 1)
+    illegal_date2 = datetime(2023, 10, 1)
+    if illegal_date1 <= date <= illegal_date2:
+        return {"illegal_period": True, "message": "Any rent raise during this time was illegal"}
+    if year not in cpi_rates or month not in cpi_rates[year]:
+        return {"error": f"No CPI data available for {year}"}
+    cpi_percent = cpi_rates[year][month]
+    if cpi_percent is None:
+        return {"error": f"There was no CPI data available for this year and month"}
+    allowed_rent = old_rent * (1 + cpi_percent)
     overcharge = new_rent - allowed_rent
     overcharge_percent = (overcharge/allowed_rent) * 100
     if overcharge_percent > 0:
