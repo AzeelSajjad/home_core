a4a5e5b5a1e225b8b5af51e3643d03ecb877053e
Azeel Sajjad <aasajjad05@gmail.com>
2025-08-31 22:42:06 -0400

    making small changes due to new additions for rent calculator

--- backend/app/api/endpoints/rent_api.py
+++ backend/app/api/endpoints/rent_api.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 from ...services.rent import rent_calculator
-from ...models.rent_model import RentRequest  
+from ...models.rent_model import RentRequest
+from ...models.cpi_data import cpi_rates  
 
 router = APIRouter()
 
 @router.post("/rent-calculate")
 def rent_calculator_endpoint(request: RentRequest):
-    result = rent_calculator(request.old_rent, request.new_rent)
+    result = rent_calculator(request.old_rent, request.new_rent, request.date, cpi_rates)
     return result
--- backend/app/models/rent_model.py
+++ backend/app/models/rent_model.py
@@ -4,4 +4,4 @@ from datetime import datetime
 class RentRequest(BaseModel):
     old_rent: float
     new_rent: float
-    
+    date: datetime
